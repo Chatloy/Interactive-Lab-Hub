053b7e2f51aede51a7304fbbe33d9af6758c2f30
Chatloy <41297715+Chatloy@users.noreply.github.com>
2021-12-10 00:34:49 -0500

    Update text2speech_example.sh

--- Lab 3/text2speech_example.sh
+++ Lab 3/text2speech_example.sh
@@ -1,3 +1,3 @@
 #from: https://elinux.org/RPi_Text_to_Speech_(Speech_Synthesis)#Festival_Text_to_Speech
 
-echo "Please pay attention to the previous mistakes, Iris!" | festival --tts
+echo "Creepy! Creepy!" | festival --tts
