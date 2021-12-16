7c6b8a9912fe1711057694f0eb60c885eb3d9a18
Chatloy <41297715+Chatloy@users.noreply.github.com>
2021-12-16 09:38:57 -0500

    Update text2speech_example.sh

--- Lab 3/text2speech_example.sh
+++ Lab 3/text2speech_example.sh
@@ -1,3 +1,3 @@
 #from: https://elinux.org/RPi_Text_to_Speech_(Speech_Synthesis)#Festival_Text_to_Speech
 
-echo "Creepy! Creepy!" | festival --tts
+echo "Hi! Chatloy! It is a great day outside! " | festival --tts
